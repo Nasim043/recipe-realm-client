a82529a0fbd71777990aadd7a6595ccd781214df
Saleh Nasim <nasimcse16@gmail.com>
2023-05-02 13:43:30 +0600

    Firebase added

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const Login = () => {
   return (
-    <div>
-      <h1>This is login page</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur maxime voluptates eligendi similique, pariatur ea.</h1>
+    <div className="my-container">
+      <div className="card w-11/12 sm:w-4/5 max-w-xl mx-auto shadow-2xl bg-base-100">
+        <div className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="email" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="password" className="input input-bordered" />
+            <label className="label">
+              <p className='label-text-alt'>Don't have an account?  <Link to='../register' className="label-text-alt link link-hover">Register</Link></p>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-outline btn-primary mb-2"><FaGoogle></FaGoogle> Login with Google</button>
+            <button className="btn btn-outline"><FaGithub></FaGithub> Login with Github</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("Inside register");
+  }
   return (
-    <div>
-      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, velit! Velit sequi laboriosam perferendis. Pariatur.</h1>
+    <div className="my-container">
+      <div className="card w-11/12 sm:w-4/5 max-w-xl mx-auto shadow-2xl bg-base-100">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input type="text" placeholder="Name" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo Url</span>
+              </label>
+              <input type="text" placeholder="Photo Url" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="text" placeholder="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="text" placeholder="password" className="input input-bordered" />
+              <label className="label">
+                <p className="label-text-alt">Already Have an Account? <Link to="../login" className="label-text-alt link link-hover">Login</Link></p>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
